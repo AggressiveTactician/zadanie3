9c5fac3b7273c44c19f86a029729758509e95ef5
Użytkownik <uytkownik@kursGIT.pl>
2020-07-18 13:12:00 +0200

    zad6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Napisz program, który pobierze od użytkownika liczbę n (typu int) i obliczy sumę szeregu harmonicznego od 1 do n,
+        // zgodnie ze wzorem poniżej:
+//        Hn = 1 + 1/2 + 1/3 + 1/4 + ... + 1/n
     }
 }
