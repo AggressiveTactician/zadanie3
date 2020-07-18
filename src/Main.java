8837e5cbd58dde31b5ae6a7ff0a5d58f090cf2f0
Użytkownik <uytkownik@kursGIT.pl>
2020-07-18 17:31:16 +0200

    zad8 z rozwiązaniem

--- src/Main.java
+++ src/Main.java
@@ -42,8 +42,10 @@ public class Main {
 //=======================================================================================================
 
 //        Napisz program realizujący prosty kalkulator.
-//                Program powinien:a.pobrać pierwszą liczbę (typu float)b.pobrać jeden ze znaków: + - / *c.pobrać drugą liczbę (typu float)d.
-//                zwrócić wynik pobranego działania Jeśli  użytkownik  poda  znak  inny  niż  obsługiwane,
+//                Program powinien:
+//                a.pobrać pierwszą liczbę (typu float)b.pobrać jeden ze znaków: + - / *
+//                c.pobrać drugą liczbę (typu float)
+//                d.zwrócić wynik pobranego działania Jeśli  użytkownik  poda  znak  inny  niż  obsługiwane,
 //        program  powinien  wypisać „Błędny znak”. Jeśli wpisanego działania nie da się zrealizować (tj. jest niezgodne z zasadami matematyki),
 //                to program powinien wypisać napis „Błąd”.
 
@@ -78,7 +80,7 @@ public class Main {
 //            }
 //        }
 
-
+        
 
     }
 }
