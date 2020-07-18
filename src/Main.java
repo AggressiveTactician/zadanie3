b15619f59c78919691ed0c8669b5328ada07af36
Użytkownik <uytkownik@kursGIT.pl>
2020-07-18 13:22:27 +0200

    zad7

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,22 @@ public class Main {
         // zgodnie ze wzorem poniżej:
         //        Hn = 1 + 1/2 + 1/3 + 1/4 + ... + 1/n
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbę n: ");
-        int number = scanner.nextInt();
-        double Hn = 0;
-        for (int i = 1; i <= number; i++) {
-            Hn = Hn + 1 / (double) i;
-        }
-        System.out.println(Hn);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Podaj liczbę n: ");
+//        int number = scanner.nextInt();
+//        double Hn = 0;
+//        for (int i = 1; i <= number; i++) {
+//            Hn = Hn + 1 / (double) i;
+//        }
+//        System.out.println(Hn);
+// =========================================================================================================
+
+//        Napisz program, który pobierze od użytkownika liczbę dodatnią (typu int) i obliczy liczbę Fibonacciego o wskazanym indeksie.
+//        Przykładowo, jeśli użytkownik poda liczbę 5, Twój program  powinien  wypisać  piątą  liczbę  Fibonacciego.
+//        Kolejne  liczby  Fibonacciego powstają  poprzez  zsumowanie  dwóch  poprzednich  liczb  Fibonacciego.
+//        Przykładowo, kilka pierwszych liczb Fibonacciego wynosi kolejno: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377...
+
+
+
     }
 }
